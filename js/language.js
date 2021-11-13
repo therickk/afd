fbf9daa4f074f1c971d78d8a0631568f67fa6427
therickk <91158248+therickk@users.noreply.github.com>
2021-11-13 18:30:55 -0500

    Delete js directory

--- js/language.js
+++
@@ -1,9 +0,0 @@
-
-$(".translate").change(function(){
-	val = $(this).val();
-    $.getJSON('js/'+val+'.json', function (json){
-	    $(".lang").each(function(index, value){
-            $(this).text($(this).attr('key'));
-	    });
-    });
-});  
